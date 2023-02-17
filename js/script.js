15ee51c86a8fafa2bc3d1a70e524a73f6f89be94
CodeKhushal <khushalsachdeva22@gmail.com>
2023-02-17 18:22:37 +0530

    Changed fade output

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function scrollFade() {
 
     var elementFourth = rect.height/4;
     var fadeInPoint = window.innerHeight - elementFourth;
-    var fadeOutPoint = -(rect.height/2);
+    var fadeOutPoint = -(rect.height/4);
 
     if (rect.top <= fadeInPoint) {
       element.classList.add('scrollFade--visible');
