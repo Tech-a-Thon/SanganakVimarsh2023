38ea0ec23ec5f767ec273a9ce7233214c18f15f5
Arpit Taneja <88012@arsd.du.ac.in>
2023-02-18 18:19:14 +0530

    Apply form designing

--- form/script.js
+++ form/script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const formBtn = document.querySelector('#submit-btn');
 const addButton = document.querySelectorAll('.add-btn');
 console.log('fjewb')
 const hidden = Array.from(document.querySelectorAll('fieldset:nth-child(-n+5):nth-child(n+2)'));
@@ -7,12 +7,21 @@ console.log('hidden', hidden);
 let playerCounter = 0
 let hiddenFieldsetCounter = 0;
 
-console.log(form)
-form.addEventListener('submit', (e) => {
-    console.log('fd')
-    e.preventDefault();
-    console.log('submitted');
-    playerCounter++;
-    console.log(hidden[hiddenFieldsetCounter]);
-    hidden[hiddenFieldsetCounter].style.display = 'block';
+// console.log(form)
+// form.addEventListener('submit', (e) => {
+//     console.log('submit')
+//     e.preventDefault();
+//     // console.log('submitted');
+//     // playerCounter++;
+//     // console.log(hidden[hiddenFieldsetCounter]);
+//     // hidden[hiddenFieldsetCounter].style.display = 'block';
+// });
+
+formBtn.addEventListener('click', (e) => {
+    console.log('clicked')
+    // e.preventDefault();
+    // playerCounter++;
+    // console.log(hidden[hiddenFieldsetCounter]);
+    // hidden[hiddenFieldsetCounter].style.display = 'block';
+    // hiddenFieldsetCounter++;
 });
